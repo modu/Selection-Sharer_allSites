749ba7365c62c008065a1231102d5883cb2dde0f
varun vyas <moduvyas@gmail.com>
2015-07-01 03:19:46 +0530

    First Commit

---
+++ contentscript.js
@@ -0,0 +1,26 @@
+var s = document.createElement('script');
+s.src = chrome.extension.getURL('selection-sharer.js');
+
+var ss = document.createElement("link");
+ss.type = "text/css";
+ss.rel = "stylesheet";
+ss.href = "selection-sharer.css";
+document.getElementsByTagName("head")[0].appendChild(ss);
+
+ window.onload = function() {
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    var code = 'alert("hello world!");';
+    try {
+      s.appendChild(document.createTextNode(code));
+      document.body.appendChild(s);
+    } catch (e) {
+      s.text = code;
+      document.body.appendChild(s);
+    }
+  }
+
+(document.head||document.documentElement).appendChild(s);
+s.onload = function() {
+    s.parentNode.removeChild(s);
+};
